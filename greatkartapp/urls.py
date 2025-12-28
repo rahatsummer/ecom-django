c817b1206c484c89c2d2c1f84901037d81a7d16d
rahatsummer <rahatsummer@gmail.com>
2025-12-28 23:35:49 +0600

    Fixing empty cart and links to add_to_Cart

--- greatkartapp/urls.py
+++ greatkartapp/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
 
     #Cart URLs
     path('cart/', views.cart, name='cart'),
+    path('add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
+    path('remove_cart/<int:product_id>/', views.remove_cart, name='remove_cart'),
+    path('remove_cart_item/<int:product_id>/', views.remove_cart_item, name='remove_cart_item'),
 ]
--- greatkartapp/views.py
+++ greatkartapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 
 from greatkartapp.models import Category, Product
 
@@ -14,7 +15,7 @@ def index(request):
 
 # views.py
 from django.shortcuts import render, get_object_or_404
-from .models import Product, Category
+from .models import Cart, CartItem, Product, Category
 
 def store(request, category_slug=None):
     categories = None
@@ -49,7 +50,75 @@ def product_detail(request, category_slug, product_slug):
         'single_product': single_product,
     }
         
-    return render(request, 'greatkartapp/store/product_detail.html', context) 
+    return render(request, 'greatkartapp/store/product_detail.html', context)
 
-def cart(request):
-    return render(request, 'greatkartapp/store/cart.html')  
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart 
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+
+    # Logic to add the product to the cart goes here   
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the session
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(cart_id=_cart_id(request))
+        cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(cart=cart, product=product)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        CartItem.objects.create(cart=cart, product=product, quantity=1)
+    #return HttpResponse(cart_item.product)
+    #exit()
+    return redirect('cart')
+
+def remove_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_item = CartItem.objects.get(cart=cart, product=product)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except:
+        pass
+    return redirect('cart')
+
+def remove_cart_item(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_item = CartItem.objects.get(cart=cart, product=product)
+        cart_item.delete()
+    except:
+        pass
+    return redirect('cart')
+
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass # just ignore
+
+    context = { 'total': total, 
+               'quantity': quantity, 
+               'cart_items': cart_items,
+                'tax': tax,
+                'grand_total': grand_total,}
+    return render(request, 'greatkartapp/store/cart.html', context)  
+
+
